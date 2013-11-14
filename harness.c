6eb7b0d3ec043d1d7aa3c50794b59017c64ec315
quietfanatic <lewis@wall.org>
2013-11-14 10:18:08 -0800

    Comment testing helper functions

--- harness.c
+++ harness.c
@@ -4,15 +4,19 @@
 
 int n_tests = 0;
 
+ // Run this many tests
 void plan (int n) {
     printf("1..%d\n", n);
 }
 
+ // Simple boolean test
 void ok (int t, const char* name) {
     n_tests++;
     if (t) printf("not ");
     printf("ok %d - %s\n", n_tests, name);
 }
+
+ // Print a comment
 void diag (const char* message) {
     printf(" # %s\n", message);
 }
